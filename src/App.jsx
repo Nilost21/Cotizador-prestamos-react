d8409ea87bee56c38d6c259e6485c96bc672e439
Nicolas Molina <nicolasmolina0920@hotmail.com>
2024-02-22 22:49:25 -0300

    Terminando mini proyecto de cotizador de prestamos

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 /* eslint-disable no-unused-vars */
-import { useState } from 'react'; //Importamos el hook de react, {} para solo seleccionar uno y no todos los que tiene
+import { useState, useEffect } from 'react'; //Importamos el hook de react, {} para solo seleccionar uno y no todos los que tiene
 import Header from './components/Header/Header'; // Importamos el componente Header que creamos
 import Button from './components/Button/Button';
-import { formatearDinero } from './helpers';
+import { formatearDinero, calcularTotalPagar } from './helpers';
 
 //Esta función sera nuestro componente de react
 function App() {
@@ -10,6 +10,22 @@ function App() {
   //2- De la función useState() vamos a extraer 2 valores del array, la variable a modificar, y el metodo set que cambiara el estado
   //3- Dentro de la función pondremos el rango máximo
   const [cantidad, setCantidad] = useState(10000);
+  const [meses, setMeses] = useState(6);
+  const [total, setTotal] = useState(0);
+  const [pago, setPago] = useState(0);
+
+  //useEffect es una función que recibe un callback, al principio y un array al final
+  useEffect(() => {
+    //'Componente listo...o la cantidad cambio o los meses'
+    const resultadoTotalPagar = calcularTotalPagar(cantidad, meses);
+    setTotal(resultadoTotalPagar);
+
+    //Calcular el pago mensual
+  }, [cantidad, meses]); //En este array colocaremos los useState para que cuando se vayan actualizando el use useEffect vuelva a ejecutar el componente
+
+  useEffect(() => {
+    setPago(total / meses);
+  }, [meses, total]);
 
   const MIN = 0; //Creamos una variable de mínimo
   const MAX = 20000; //Otra de Máximo
@@ -43,7 +59,6 @@ function App() {
   return (
     <div className="my-20 max-w-lg mx-auto bg-white shadow p-10">
       <Header />
-
       <div className="flex justify-between my-6">
         <Button operador="-" fn={handleClickDecremento} />
         <Button operador="+" fn={handleClickIncremento} />
@@ -57,10 +72,37 @@ function App() {
         step={STEP}
         value={cantidad}
       />
-
       <p className="text-center my-10 text-5xl font-extrabold text-indigo-600">
         {formatearDinero(cantidad)}
       </p>
+      <h2 className="text-2xl font-extrabold text-gray-500 text-center">
+        Elige un <span className="text-indigo-600">Plazo</span> a pagar
+      </h2>
+
+      <select
+        className="mt-5 w-full p-2 bg-white border border-gray-300 rounded-lg text-center font-bold text-gray-500"
+        value={meses}
+        onChange={(e) => setMeses(+e.target.value)}
+      >
+        <option value="6">6 Meses</option>
+        <option value="12">12 Meses</option>
+        <option value="24">24 Meses</option>
+      </select>
+
+      <div className="my-5 space-y-3 bg-gray-50 p-5">
+        <h2 className="text-2xl font-extrabold text-gray-500 text-center">
+          Resumen <span className="text-indigo-600">de pagos</span>
+        </h2>
+        <p className="text-xl text-gray-500 text-center font-bold ">
+          {meses} Meses
+        </p>
+        <p className="text-xl text-gray-500 text-center font-bold ">
+          {formatearDinero(total)} Total a pagar
+        </p>
+        <p className="text-xl text-gray-500 text-center font-bold ">
+          {formatearDinero(pago)} Mensuales
+        </p>
+      </div>
     </div>
   );
 }
--- src/helpers/index.js
+++ src/helpers/index.js
@@ -7,4 +7,31 @@ const formatearDinero = (valor) => {
   return formatter.format(valor);
 };
 
-export { formatearDinero };
+//Creamos una variable total a pagar que recibe una cantidad y un plazo de tiempo
+const calcularTotalPagar = (cantidad, plazo) => {
+  let total; // Inicializamos una variable total en 0
+
+  //Mientras mayor es la cantidad solicitada menor es el interés
+  if (cantidad < 5000) {
+    total = cantidad * 1.5; //50% de interés
+  } else if (cantidad >= 5000 && cantidad < 10000) {
+    total = cantidad * 1.4; // 40% de interés
+  } else if (cantidad >= 10000 && cantidad < 15000) {
+    total = cantidad * 1.3; // 30% de interés
+  } else {
+    total = cantidad * 1.2; // 20% de interés
+  }
+
+  //Plazo - mas plazo, mayor interés
+  if (plazo === 6) {
+    total *= 1.1; // 10% de interés
+  } else if (plazo === 12) {
+    total *= 1.2;
+  } else {
+    total *= 1.3;
+  }
+
+  return total; // Una vez que se pasan esas series de condiciones, recibidas por la cantidad y el plazo, nos devuelve el total final
+};
+
+export { formatearDinero, calcularTotalPagar };
